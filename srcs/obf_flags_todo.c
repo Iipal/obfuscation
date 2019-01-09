b014b021f06f0f8d9b631261dd06dab2b29a45bc
_ipal <malkoleyplay@gmail.com>
2019-01-09 15:28:37 +0200

    added functional for other flags

--- srcs/obf_flags_todo.c
+++ srcs/obf_flags_todo.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/09 14:21:37 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/01/09 14:40:53 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/01/09 15:27:02 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,24 +22,20 @@ bool	obf_flag_doall(t_file **file)
 
 bool	obf_flag_wss_concat(t_file **file)
 {
-	if (*file)
-		printf("-wc\n");
+	_NOTIS_MSG(_ERRNO_FILE_INVALID_, *file = obf_file_cut_whitespaces(*file));
+	_NOTIS_MSG(_ERRNO_FILE_OBFUSCT_, *file = obf_file_concat(*file));
 	return (true);
 }
 
 bool	obf_flag_wss_ccrot(t_file **file)
 {
-	if (*file)
-		printf("-wr\n");
+	_NOTIS_MSG(_ERRNO_FILE_INVALID_, *file = obf_file_cut_whitespaces(*file));
+	_NOTIS_MSG("", obf_file_ccrot(*file));
 	return (true);
 }
 
 bool	obf_flag_wss(t_file **file)
 {
-	if (*file)
-		printf("-wo\n");
+	_NOTIS_MSG(_ERRNO_FILE_INVALID_, *file = obf_file_cut_whitespaces(*file));
 	return (true);
 }
-
-// _NOTIS_MSG(_ERRNO_FILE_INVALID_, file = obf_file_cut_whitespaces(file));
-// _NOTIS_MSG(_ERRNO_FILE_OBFUSCT_, file = obf_file_concat(file));
