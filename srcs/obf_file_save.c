95db85203a9f2693451b734373665ad86e229ae7
_ipal <malkoleyplay@gmail.com>
2019-01-09 15:44:26 +0200

    fixed error message

--- srcs/obf_file_save.c
+++ srcs/obf_file_save.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/09 10:55:17 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/01/09 15:08:10 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/01/09 15:39:38 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ bool	obf_file_save(t_file *file, string src_file_name)
 	file_name = ft_strdup(_FILE_SAVE_PREF_);
 	file_name = ft_strcat(file_name, src_file_name);
 	_MSG("Saving to file \'"); _MSG(file_name); write(1, "\': ", _RSIZEOF(3));
-	_NOTIS_FMSG(_ERR_INF_FILE_REOPEN_, !(!(fd = open(file_name, O_RDWR | O_CREAT, 0644)) || fd < 0));
+	_NOTIS_FMSG(_ERRNO_FILE_OPENING_, !(!(fd = open(file_name, O_RDWR | O_CREAT, 0644)) || fd < 0));
 	while (++i < file->lines)
 	{
 		write(fd, file->tab[i], _RSIZEOF(strlen(file->tab[i])));
-		write(fd, "\n", sizeof(char));
+		write(fd, "\n", _RSIZEOF(1));
 	}
 	close(fd);
 	ft_strdel(&file_name);
