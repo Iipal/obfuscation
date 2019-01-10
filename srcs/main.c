144d40d69d9cdde26cb97e701deb316254bebf85
_ipal <malkoleyplay@gmail.com>
2019-01-10 11:26:11 +0200

    fixed sigfault

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/30 00:29:31 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/01/10 10:41:44 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/01/10 11:17:56 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ static bool	obf_flag_validate(string flags, string src_flags)
 	return (true);
 }
 
-static bool	obf_flags_parser(string flags, t_file *file)
+static bool	obf_flags_parser(string flags, t_file **file)
 {
 	const fptr_flags	flags_funcs[] = {&obf_flag_obfusct, &obf_flag_ccrot, &obf_flag_wss, &obf_flag_fake};
 	char				flags_all[] = {FLAGS_OBFUSCT, FLAGS_CAESARC, FLAGS_WHITESS, FLAGS_FAKEING};
@@ -40,6 +40,7 @@ static bool	obf_flags_parser(string flags, t_file *file)
 	int					j = NEG;
 	
 	src_flags = flags;
+	
 	while (*flags == '-')
 		++flags;
 	while (*flags && (j = NEG))
@@ -48,7 +49,7 @@ static bool	obf_flags_parser(string flags, t_file *file)
 			if (*flags == flags_all[j] && (valid_flag = true))
 			{
 				_NOTIS_F(obf_flag_validate(flags, src_flags));
-				_NOTIS_MSG(_ERRNO_FILE_OBFUSCT_, flags_funcs[j](&file));
+				_NOTIS_MSG(_ERRNO_FILE_OBFUSCT_, flags_funcs[j](file));
 			}
 		(!valid_flag) ? (bool)printf("%c invalid option.\n", *flags) : (valid_flag = !valid_flag);
 		++(flags);
@@ -72,7 +73,7 @@ int			main(int argc, char *argv[])
 			write(1, "\t", _RSIZEOF(1)); _MSG(argv[i + 1]); write(1, ":\n", _RSIZEOF(2));
 			_NOTIS_MPE(_ERRNO_FILE_OPENING_, !(!(fd = open(argv[i + 1], O_RDONLY)) || fd < 0));
 			_NOTIS_MSG(_ERRNO_FILE_READING_, file = obf_file_reader(&fd, argv[i + 1]));
-			_NOTIS_MSG(_ERRNO_FLAG_PARSING_, obf_flags_parser(*argv, file));
+			_NOTIS_MSG(_ERRNO_FLAG_PARSING_, obf_flags_parser(*argv, &file));
 			_NOTIS_MSG(_ERRNO_FILE_OSAVING_, obf_file_save(file, argv[i + 1]));
 			obf_file_free(file);
 		}
